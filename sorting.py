95e7b4789c61bf82c332ea3ebce5db0f53e67d72
Adam Jakubiak <a.jakubiak.srem@gmail.com>
2024-08-29 17:32:33 +0200

    Added gui

--- sorting.py
+++ sorting.py
@@ -1,25 +1,58 @@
 import os, shutil, sys
-from win10toast import ToastNotifier
+from tkinter import *
+import customtkinter
+from tkinter import filedialog
+from CTkMessagebox import CTkMessagebox
 
-toaster = ToastNotifier()
-
-path_to_files = 'F:/Captures' #Enter the path here to folder
+root = customtkinter.CTk()
+root.geometry("300x500")
+root.title("File Sorter")
 n = 0
-try: 
-    for subdir, dir, files in os.walk(path_to_files):
-        for element in files:
-            separator = ' ' #Select separator of name. for exmample: _ ; Space can be used
-            try:
-                folders = element.split(separator)[0] 
-            except IndexError:
-                toaster.show_toast('Too high number', 'You choose too high number is square bracket')
-                sys.exit()
-            if not os.path.exists(f'{path_to_files}/{folders}'):
-                os.mkdir(f'{path_to_files}/{folders}')
-        for file in files:
-            if file == 'desktop.ini':
-                continue
-            shutil.move(path_to_files + '/' + file, path_to_files +'/' + file.split(' ')[0] + '/' + file) #moving file
-            n += 1
-except FileNotFoundError:
-    toaster.show_toast('Files have been sorted', f'{n} files sorted', duration=4)
+def open_dir_selector():
+    folder_path = filedialog.askdirectory()
+    current_dir.set(folder_path)
+
+def sort_files():
+    global n
+    current_dir.set(current_dir.get().replace("\\", "/"))
+    try:
+        for subdir, dir, files in os.walk(current_dir.get()):
+            for element in files: #Select separator of name. for example: _ ; Space can be used
+                try:
+                    folders = element.split(separator.get())[int(prefix.get())]
+                except IndexError:
+                    CTkMessagebox(title="Error", message=f"Wrong prefix", icon="cancel")
+
+
+                if not os.path.exists(f'{current_dir.get()}/{folders}'):
+                    pass
+                    os.mkdir(f'{current_dir.get()}/{folders}')
+
+            for file in files:
+                if file == 'desktop.ini':
+                    continue
+                shutil.move(current_dir.get() + '/' + file, current_dir.get() + '/' + file.split(separator.get())[int(prefix.get())] + '/' + file) #moving file
+                n += 1
+
+    except FileNotFoundError:
+        CTkMessagebox(title="Files sorted", message=f"{n} have been sorted", icon="check")
+
+#TK Variables
+current_dir = StringVar()
+separator = StringVar()
+prefix = StringVar()
+prefix.set("0")
+
+#Tkinter Widgets
+customtkinter.CTkButton(master=root, text="Select folder", command=open_dir_selector).place(relx=0.5, rely=0.1, anchor=CENTER)
+customtkinter.CTkLabel(master=root, text="Directory path").place(relx=0.5, rely=0.2, anchor=CENTER)
+customtkinter.CTkEntry(master=root, textvariable=current_dir).place(relx=0.5, rely=0.25, anchor=CENTER)
+customtkinter.CTkLabel(master=root, text="Separator").place(relx=0.5, rely=0.35, anchor=CENTER)
+customtkinter.CTkEntry(master=root, textvariable=separator).place(relx=0.5, rely=0.4, anchor=CENTER)
+customtkinter.CTkLabel(master=root, text="Prefix").place(relx=0.5, rely=0.5, anchor=CENTER)
+customtkinter.CTkEntry(master=root, textvariable=prefix).place(relx=0.5, rely=0.55, anchor=CENTER)
+customtkinter.CTkButton(master=root, text="Sort files", command=sort_files).place(relx=0.5, rely=0.75, anchor=CENTER)
+
+
+
+root.mainloop()
